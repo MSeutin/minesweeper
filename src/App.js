a7c70a50ec74c57cad7610966f81bb16b1c3d3a6
Michael Seutin <frenchmike@gmail.com>
2024-02-19 21:37:18 -0800

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import Home from "./components/Home"
+import { useState, useEffect } from "react";
+import Home from "./components/Home";
+import { Box, Typography, Button } from "@mui/material";
+import BeginnerGrid from "./components/beginner/BeginnerGrid";
+import IntermediateGrid from "./components/intermediate/IntermediateGrid";
 
 function App() {
+  const [level, setLevel] = useState(null);
   return (
-    <div>
-      <Home />
-    </div>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      flexDirection="column"
+      sx={{ width: "100vw", height: "100vh", bgcolor: "#f2f2f2" }}
+    >
+      <Typography
+        variant="h2"
+        color="primary"
+        sx={{ alignSelf: "flex-start", marginLeft: "3rem", marginTop: "1rem"}}
+      >
+        Minesweeper
+      </Typography>
+      <div
+        style={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: "5rem",
+        }}
+      >
+        {level === null && (
+          <>
+            <Button
+              variant="outlined"
+              color="primary"
+              size="large"
+              onClick={() => setLevel("beginner")}
+            >
+              Beginner
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              size="large"
+              onClick={() => setLevel("intermediate")}
+            >
+              Intermediate
+            </Button>
+          </>
+        )}
+        {level === "beginner" && <BeginnerGrid />}
+        {level === "intermediate" && <IntermediateGrid />}  
+      </div>
+    </Box>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,26 @@
 import BeginnerGrid from "./beginner/BeginnerGrid"
 import IntermediateGrid from "./intermediate/IntermediateGrid"
-import Button from "@mui/material/Button"
+import { Button, Box } from "@mui/material"
 
 function Home() {
     return (
-      <>
-            <h1>Minesweeper</h1>
-            <h2>Choose a difficulty</h2>
-            <Button variant="contained" color="primary" href="/beginner">
-                <BeginnerGrid />
-            </Button>
-            <Button variant="contained" color="primary" href="/intermediate">
-                <IntermediateGrid />
-            </Button>
-      </>
-      
-  )
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-around"
+        flexDirection="column"
+        sx={{ width: "100%", height: "100%", bgcolor: "lightblue" }}
+      >
+        <h1>Minesweeper</h1>
+        <Box display="flex" gap={4}>
+          <Button variant="contained" color="primary" href="/beginner">
+            <BeginnerGrid />
+          </Button>
+          <Button variant="contained" color="primary" href="/intermediate">
+            <IntermediateGrid />
+          </Button>
+        </Box>
+      </Box>
+    );
 }
 export default Home
